18f481f7401caf68d7883393ea23842cacd66a9b
DenysAbramets <abramets_denys@outlook.com>
2023-07-21 12:45:25 +0200

    add env file and DB_HOST

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// const DB_HOST =
+//   "mongodb+srv://abramets:p4Jz0EUHoodLcAvK@cluster0.yjl44kj.mongodb.net/";
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
